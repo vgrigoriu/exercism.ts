13804110b184f4663cc8ee25ebd1f6f1192bb493
Victor Grigoriu <vgrigoriu@gmail.com>
2019-01-19 14:17:40 +0200

    Strain solution

--- strain/strain.ts
+++ strain/strain.ts
@@ -1,8 +1,21 @@
-function keep<T>(input: Array<T>, _: (item: T) => boolean): Array<T> {
-    return input
+function keep<T>(input: Array<T>, predicate: (item: T) => boolean): Array<T> {
+    let result = new Array<T>()
+    for (const item of input) {
+        if (predicate(item)) {
+            result.push(item)
+        }
+    }
+    return result
 }
-function discard<T>(input: Array<T>, _: (item: T) => boolean): Array<T> {
-    return input
+function discard<T>(
+    input: Array<T>,
+    predicate: (item: T) => boolean
+): Array<T> {
+    return keep(input, not(predicate))
+}
+
+function not<T>(predicate: (_: T) => boolean): (_: T) => boolean {
+    return x => !predicate(x)
 }
 
 export { keep, discard }
