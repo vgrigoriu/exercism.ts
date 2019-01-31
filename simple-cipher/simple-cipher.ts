e45812761fc71efc296aa4fe839a695a099e6687
Victor Grigoriu <vgrigoriu@gmail.com>
2019-01-31 21:09:42 +0200

    Simple Cipher: clearer? solution

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -1,20 +1,20 @@
 class SimpleCipher {
     key: string
+
     constructor(key: string = SimpleCipher.randomKey()) {
         if (!key.match(/^[a-z]+$/)) {
             throw "Bad key"
         }
         this.key = key
     }
+
     encode(plain: string): string {
         let encrypted = ""
         const a = "a".charCodeAt(0)
         for (let i = 0; i < plain.length; i++) {
-            const char = plain[i]
-            const keyChar = this.key[i % this.key.length]
-            encrypted += String.fromCodePoint(
-                a + ((char.charCodeAt(0) - a + keyChar.charCodeAt(0) - a) % 26)
-            )
+            const char = plain[i].charCodeAt(0) - a
+            const keyChar = this.key[i % this.key.length].charCodeAt(0) - a
+            encrypted += String.fromCodePoint(a + ((char + keyChar) % 26))
         }
         return encrypted
     }
@@ -23,11 +23,9 @@ class SimpleCipher {
         let plain = ""
         const a = "a".charCodeAt(0)
         for (let i = 0; i < encrypted.length; i++) {
-            const char = encrypted[i]
-            const keyChar = this.key[i % this.key.length]
-            plain += String.fromCodePoint(
-                a + ((char.charCodeAt(0) - keyChar.charCodeAt(0) + 26) % 26)
-            )
+            const char = encrypted[i].charCodeAt(0) - a
+            const keyChar = this.key[i % this.key.length].charCodeAt(0) - a
+            plain += String.fromCodePoint(a + (char - keyChar + 26) % 26)
         }
         return plain
     }
