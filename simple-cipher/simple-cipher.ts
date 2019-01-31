43024ca9b66e3b6e9ab09f6c7a5d827d7051b931
Victor Grigoriu <vgrigoriu@gmail.com>
2019-01-31 21:03:06 +0200

    Simple Cipher solution

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -1,10 +1,47 @@
 class SimpleCipher {
-    encode( /* Parameters go here */ ) {
-        // Your code here
+    key: string
+    constructor(key: string = SimpleCipher.randomKey()) {
+        if (!key.match(/^[a-z]+$/)) {
+            throw "Bad key"
+        }
+        this.key = key
+    }
+    encode(plain: string): string {
+        let encrypted = ""
+        const a = "a".charCodeAt(0)
+        for (let i = 0; i < plain.length; i++) {
+            const char = plain[i]
+            const keyChar = this.key[i % this.key.length]
+            encrypted += String.fromCodePoint(
+                a + ((char.charCodeAt(0) - a + keyChar.charCodeAt(0) - a) % 26)
+            )
+        }
+        return encrypted
     }
 
-    decode( /* Parameters go here */ ) {
-        // Your code here
+    decode(encrypted: string): string {
+        let plain = ""
+        const a = "a".charCodeAt(0)
+        for (let i = 0; i < encrypted.length; i++) {
+            const char = encrypted[i]
+            const keyChar = this.key[i % this.key.length]
+            plain += String.fromCodePoint(
+                a + ((char.charCodeAt(0) - keyChar.charCodeAt(0) + 26) % 26)
+            )
+        }
+        return plain
+    }
+
+    private static randomKey(): string {
+        const allLetters = "abcdefghijklmnopqrstuvwxyz"
+        let key = ""
+        for (let i = 0; i < 100; i++) {
+            key += allLetters.charAt(
+                Math.floor(Math.random() * allLetters.length)
+            )
+        }
+
+        return key
     }
 }
 
