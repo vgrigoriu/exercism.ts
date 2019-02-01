f0f701b4c1e87e1944bdbaa24a29cf776547d17f
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-01 20:16:04 +0200

    Triangle solution

--- triangle/triangle.ts
+++ triangle/triangle.ts
@@ -1,12 +1,32 @@
 export default class Triangle {
-
     sides: number[]
 
     constructor(...sides: number[]) {
-        this.sides = sides
+        // because otherwise 10 < 1, because javascript
+        this.sides = sides.sort(function(x: number, y: number) {
+            return x - y
+        })
     }
 
-    kind() {
-        return ""
+    kind(): string {
+        if (this.sides[0] <= 0) {
+            throw "sides have to be positive"
+        }
+        if (this.sides[0] + this.sides[1] < this.sides[2]) {
+            throw "not a triangle"
+        }
+        if (
+            this.sides[0] === this.sides[1] &&
+            this.sides[1] === this.sides[2]
+        ) {
+            return "equilateral"
+        }
+        if (
+            this.sides[0] === this.sides[1] ||
+            this.sides[1] === this.sides[2]
+        ) {
+            return "isosceles"
+        }
+        return "scalene"
     }
 }
