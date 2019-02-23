1b98151703ce84fc007560c197db813518beccc3
Victor Grigoriu <vgrigoriu@gmail.com>
2019-02-23 10:02:03 +0200

    Rotational Cipher solution

--- rotational-cipher/rotational-cipher.ts
+++ rotational-cipher/rotational-cipher.ts
@@ -1,1 +1,20 @@
-export default class RotationalCipher {}
+export default class RotationalCipher {
+    static rotate(plain: string, key: number) {
+        let encrypted = ""
+        for (let index = 0; index < plain.length; index++) {
+            encrypted += rotateChar(plain[index], key)
+        }
+
+        return encrypted
+    }
+}
+
+function rotateChar(c: string, key: number): string {
+    if (c < "A" || ("Z" < c && c < "a") || "z" < c) {
+        // not a letter
+        return c
+    }
+    const char = c.charCodeAt(0)
+    const a = ("a" <= c && c <= "z" ? "a" : "A").charCodeAt(0)
+    return String.fromCharCode(a + ((char - a + key) % 26))
+}
