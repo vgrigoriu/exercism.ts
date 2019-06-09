f23cfff3b85e64dbfc75af1a2a64dd89effa8019
Victor Grigoriu <vgrigoriu@gmail.com>
2019-06-09 18:22:26 +0300

    Moe const to top level

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -1,4 +1,5 @@
 const ALL_LETTERS = "abcdefghijklmnopqrstuvwxyz"
+const A = "a".charCodeAt(0)
 
 class SimpleCipher {
     key: string
@@ -12,22 +13,20 @@ class SimpleCipher {
 
     encode(plain: string): string {
         let encrypted = ""
-        const a = "a".charCodeAt(0)
         for (let i = 0; i < plain.length; i++) {
-            const char = plain[i].charCodeAt(0) - a
-            const keyChar = this.key[i % this.key.length].charCodeAt(0) - a
-            encrypted += String.fromCodePoint(a + ((char + keyChar) % 26))
+            const char = plain[i].charCodeAt(0) - A
+            const keyChar = this.key[i % this.key.length].charCodeAt(0) - A
+            encrypted += String.fromCodePoint(A + ((char + keyChar) % 26))
         }
         return encrypted
     }
 
     decode(encrypted: string): string {
         let plain = ""
-        const a = "a".charCodeAt(0)
         for (let i = 0; i < encrypted.length; i++) {
-            const char = encrypted[i].charCodeAt(0) - a
-            const keyChar = this.key[i % this.key.length].charCodeAt(0) - a
-            plain += String.fromCodePoint(a + ((char - keyChar + 26) % 26))
+            const char = encrypted[i].charCodeAt(0) - A
+            const keyChar = this.key[i % this.key.length].charCodeAt(0) - A
+            plain += String.fromCodePoint(A + ((char - keyChar + 26) % 26))
         }
         return plain
     }
