a0008c9994784257605ff7edcdecfb207f1934f6
Victor Grigoriu <vgrigoriu@gmail.com>
2019-06-14 14:54:36 +0300

    RNA transription solution

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,7 +1,25 @@
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
+    toRna(dna: string): string {
+        return dna
+            .split("")
+            .map(transcribe)
+            .join("")
     }
+}
+
+const map: { [nucleotide: string]: string } = {
+    C: "G",
+    G: "C",
+    A: "U",
+    T: "A"
+}
+
+function transcribe(nucleotide: string): string {
+    if (!map.hasOwnProperty(nucleotide)) {
+        throw "Invalid input DNA."
+    }
+
+    return map[nucleotide]
 }
 
 export default Transcriptor
