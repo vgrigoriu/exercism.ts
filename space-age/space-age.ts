36532a10231f2caec9d874545aaa356e1a477e1c
Victor Grigoriu <vgrigoriu@gmail.com>
2019-09-23 17:49:07 +0300

    test 1

---
+++ space-age/space-age.ts
@@ -0,0 +1,39 @@
+export default class SpaceAge {
+    seconds: number
+
+    constructor(earthSeconds: number) {
+        this.seconds = earthSeconds
+    }
+
+    onEarth(): number {
+        return -1
+    }
+
+    onMercury(): number {
+        return -1
+    }
+
+    onVenus(): number {
+        return -1
+    }
+
+    onMars(): number {
+        return -1
+    }
+
+    onJupiter(): number {
+        return -1
+    }
+
+    onSaturn(): number {
+        return -1
+    }
+
+    onUranus(): number {
+        return -1
+    }
+
+    onNeptune(): number {
+        return -1
+    }
+}
