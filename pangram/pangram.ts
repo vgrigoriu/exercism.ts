cc27319db17116169b3fbf545fe307acb5bd9803
Victor Grigoriu <vgrigoriu@gmail.com>
2019-10-20 17:59:37 +0300

    test 1

---
+++ pangram/pangram.ts
@@ -0,0 +1,11 @@
+export default class Pangram {
+    sentence: string;
+
+    constructor(sentence: string) {
+        this.sentence = sentence
+    }
+
+    isPangram(): boolean {
+        return false
+    }
+}
