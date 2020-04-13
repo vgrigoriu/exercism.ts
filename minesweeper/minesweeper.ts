70dbfb0b9b1ba8174e648483569e9d477e6072f1
Victor Grigoriu <vgrigoriu@gmail.com>
2020-04-13 10:11:21 +0300

    move methods to class

--- minesweeper/minesweeper.ts
+++ minesweeper/minesweeper.ts
@@ -5,44 +5,45 @@ export default class Minesweeper {
             const row = table[r]
             let newRow = ""
             for (let c = 0; c < row.length; c++) {
-                newRow += annotateCell(table, r, c)
+                newRow += this.annotateCell(table, r, c)
             }
             result.push(newRow)
         }
 
         return result
     }
-}
 
-function annotateCell(table: string[], r: number, c: number): string {
-    if (isBomb(table, r, c)) {
-        return "*"
-    }
 
-    let noOfBombs = 0
-    for (let i = r - 1; i <= r + 1; i++) {
-        for (let j = c - 1; j <= c + 1; j++) {
-            // it's ok to count table[r][c] since we checked above that it's not *
-            if (isBomb(table, i, j)) {
-                noOfBombs++
+    private annotateCell(table: string[], r: number, c: number): string {
+        if (this.isBomb(table, r, c)) {
+            return "*"
+        }
+
+        let noOfBombs = 0
+        for (let i = r - 1; i <= r + 1; i++) {
+            for (let j = c - 1; j <= c + 1; j++) {
+                // it's ok to count table[r][c] since we checked above that it's not *
+                if (this.isBomb(table, i, j)) {
+                    noOfBombs++
+                }
             }
         }
+
+        if (noOfBombs === 0) {
+            return " "
+        }
+
+        return String(noOfBombs)
     }
 
-    if (noOfBombs === 0) {
-        return " "
-    }
+    private isBomb(table: string[], r: number, c: number): boolean {
+        if (r < 0 || table.length <= r) {
+            return false
+        }
+        if (c < 0 || table[r].length <= c) {
+            return false
+        }
 
-    return String(noOfBombs)
-}
-
-function isBomb(table: string[], r: number, c: number): boolean {
-    if (r < 0 || table.length <= r) {
-        return false
+        return table[r][c] === "*"
     }
-    if (c < 0 || table[r].length <= c) {
-        return false
-    }
-
-    return table[r][c] === "*"
 }
